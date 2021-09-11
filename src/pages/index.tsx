6ff207cdfb7a11b5794b3a19f3eb69cfe8c7521a
Jonathan Alba Videira <jonathan.videira@gmail.com>
2021-09-11 17:12:40 -0300

    Styling home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,29 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Início - ig.news</title>
       </Head>
-      <h1>
-        Hello <span>World</span>
-      </h1>
+      <main className={styles.container}>
+        <article className={styles.hero}>
+          <p>👏 Hey, welcome</p>
+          <h1>
+            News about
+            <br />
+            the <span>React</span> world
+          </h1>
+          <p>
+            Get access to all publications
+            <br />
+            <span>for $9.90/month</span>
+          </p>
+        </article>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
