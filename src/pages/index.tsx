85c103ac08366ded6a7ad004258bc36418f0e83e
Jonathan Alba Videira <jonathan.videira@gmail.com>
2021-09-11 21:19:09 -0300

    Retriving price information from stripe api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+type HomeProps = {
+  product: {
+    priceId: string;
+    amount: string;
+  };
+};
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -20,12 +29,28 @@ export default function Home() {
           <p>
             Get access to all publications
             <br />
-            <span>for $9.90/month</span>
+            <span>for {product.amount}/month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </article>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const price = await stripe.prices.retrieve('price_1JYfbaKMMxmpALteEaycAnC8');
+
+  return {
+    props: {
+      product: {
+        priceId: price.id,
+        amount: new Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: 'USD',
+        }).format(price.unit_amount / 100),
+      },
+    },
+  };
+};
