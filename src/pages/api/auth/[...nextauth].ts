756482dd94cb0b392004a9a734c0a73605446ac2
Jonathan Alba Videira <jonathan.videira@gmail.com>
2021-09-15 08:37:12 -0300

    Configuring faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,6 @@ import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
