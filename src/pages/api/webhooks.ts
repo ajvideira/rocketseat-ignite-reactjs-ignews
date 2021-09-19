d5786a047d9480a0ae22d853e3a8d444796463a6
Jonathan Alba Videira <jonathan.videira@gmail.com>
2021-09-18 23:24:37 -0300

    Listening stripe events

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,9 +1,47 @@
+import { buffer } from "micro";
 import { NextApiRequest, NextApiResponse } from "next";
+import Stripe from "stripe";
+
+import { stripe } from "../../services/stripe";
+
+const relevantEvents = new Set(["checkout.session.completed"]);
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 export default async function webhooks(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log("Evento recebido.");
-  return res.status(200).json({ ok: true });
+  if (req.method !== "POST") {
+    return res
+      .setHeader("Allow", ["POST"])
+      .status(405)
+      .end("Method Not Allowed");
+  }
+
+  const bufferedRequest = await buffer(req);
+
+  const stripeSignature = req.headers["stripe-signature"];
+
+  let event: Stripe.Event;
+  try {
+    event = stripe.webhooks.constructEvent(
+      bufferedRequest.toString(),
+      stripeSignature,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end(`Webhook error: ${error.message}`);
+  }
+
+  if (relevantEvents.has(event.type)) {
+    console.log("Evento recebido", event);
+  }
+
+  return res.json({ received: true });
 }
