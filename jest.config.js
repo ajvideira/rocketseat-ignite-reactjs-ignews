ac0c6373ac105eefca333b6032a56899efa497ab
Jonathan Alba Videira <jonathan.videira@gmail.com>
2022-03-14 21:01:52 -0300

    Configuring jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testIgnorePatterns: ["/node_modules/", "/.next/"],
+  setupFilesAfterEnv: ["<rootdir>/src/tests/setupTests.ts"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootdir>/node_modules/babel-jest",
+  },
+  textEnvironment: "jsdom",
+};
