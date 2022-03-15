638907524664ad80736a7a3c559572e426e451f4
Jonathan Alba Videira <jonathan.videira@gmail.com>
2022-03-15 09:13:27 -0300

    Creating tests for SigninButton component

---
+++ src/components/SignInButton/SigninButton.spec.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import { SignInButton } from ".";
+import { mocked } from "ts-jest/utils";
+import { useSession } from "next-auth/client";
+
+jest.mock("next-auth/client");
+
+describe("SigninButton Component", () => {
+  it("should renders correctly when not authenticated", () => {
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce([null, false]);
+
+    render(<SignInButton />);
+
+    expect(screen.getByText("Sign in with GitHub")).toBeInTheDocument();
+  });
+
+  it("should renders correctly when authenticated", () => {
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce([
+      {
+        user: {
+          name: "John Doe",
+          email: "john.doe@example.com",
+          image: "https://google.com",
+        },
+        expires: "fake-expires",
+      },
+      false,
+    ]);
+
+    render(<SignInButton />);
+
+    expect(screen.getByText("John Doe")).toBeInTheDocument();
+  });
+});
