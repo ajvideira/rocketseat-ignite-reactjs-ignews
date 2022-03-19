d1517d4c0c6164a36c9518713d2863931c1cae2c
Jonathan Alba Videira <jonathan.videira@gmail.com>
2022-03-18 22:09:37 -0300

    Seeing coverage of tests; Test of stripe in progress

--- src/components/SignInButton/SigninButton.spec.tsx
+++ src/components/SignInButton/SigninButton.spec.tsx
@@ -1,6 +1,6 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { SignInButton } from ".";
-import { useSession } from "next-auth/client";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 jest.mock("next-auth/client");
 
@@ -33,5 +33,43 @@ describe("SigninButton Component", () => {
     render(<SignInButton />);
 
     expect(screen.getByText("John Doe")).toBeInTheDocument();
+  });
+
+  it("should call signIn when user click on the button and is not authenticated", () => {
+    const useSessionMocked = jest.mocked(useSession);
+    useSessionMocked.mockReturnValueOnce([null, false]);
+
+    const signInMocked = jest.mocked(signIn);
+
+    render(<SignInButton />);
+
+    const button = screen.getByText("Sign in with GitHub");
+    fireEvent.click(button);
+
+    expect(signInMocked).toHaveBeenCalled();
+  });
+
+  it("should call signOut when user click on the button and is authenticated", () => {
+    const useSessionMocked = jest.mocked(useSession);
+    useSessionMocked.mockReturnValueOnce([
+      {
+        user: {
+          name: "John Doe",
+          email: "john.doe@example.com",
+          image: "https://google.com",
+        },
+        expires: "fake-expires",
+      },
+      false,
+    ]);
+
+    const signOutMocked = jest.mocked(signOut);
+
+    render(<SignInButton />);
+
+    const button = screen.getByText("John Doe");
+    fireEvent.click(button);
+
+    expect(signOutMocked).toHaveBeenCalled();
   });
 });
--- src/components/SubscribeButton/SubscribeButton.spec.tsx
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -2,11 +2,20 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import { SubscribeButton } from ".";
 import { useSession, signIn } from "next-auth/client";
 import { useRouter } from "next/router";
+import AxiosMock from "axios-mock-adapter";
+import { api } from "../../services/api";
+import { getStrypeJs } from "../../services/stripe-js";
 
 jest.mock("next-auth/client");
 jest.mock("next/router");
+jest.mock("../../services/stripe-js");
+
+const apiMock = new AxiosMock(api);
 
 describe("SubscribeButton Component", () => {
+  beforeEach(() => {
+    apiMock.reset();
+  });
   it("should renders correctly", () => {
     const useSessionMocked = jest.mocked(useSession);
     useSessionMocked.mockReturnValueOnce([null, false]);
@@ -59,4 +68,40 @@ describe("SubscribeButton Component", () => {
 
     expect(pushMocked).toHaveBeenCalledWith("/posts");
   });
+
+  /*it("should redirect to stripe website when click to subscribe and not have yet a subscription", async () => {
+    const useSessionMocked = jest.mocked(useSession);
+    useSessionMocked.mockReturnValueOnce([
+      {
+        user: {
+          name: "John Doe",
+          email: "john.doe@example.com",
+          image: "https://google.com",
+        },
+        activeSubscription: null,
+        expires: "fake-expires",
+      },
+      false,
+    ]);
+
+    apiMock.onPost("/subscribe").reply(200, {
+      data: {
+        sessionId: "fake-session-id",
+      },
+    });
+
+    const redirectToCheckoutMocked = jest.fn();
+
+    const stripeMocked = jest.mocked(getStrypeJs);
+    stripeMocked.mockReturnValueOnce({
+      redirectToCheckout: redirectToCheckoutMocked,
+    } as any);
+
+    render(<SubscribeButton />);
+
+    const button = screen.getByText("Subscribe now");
+    fireEvent.click(button);
+
+    expect(redirectToCheckoutMocked).toHaveBeenCalled();
+  });*/
 });
